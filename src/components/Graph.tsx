c6d12de4a9259c6b5b97fdfd4d60bcb0e2843e66
Melih Gunal <gunalmel@med.umich.edu>
2025-01-21 16:39:56 -0500

    action filtering logic is implemented

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,13 +1,17 @@
 import React, {useEffect, useRef, useState} from "react";
 import Plotly, {Layout, ScatterData} from 'plotly.js-basic-dist';
-import data from '../sample-data/data.json';
-import layout from '../sample-data/layout.json';
 import config from '../sample-data/config.json';
+import filterActions from "../filter-actions";
 
 const Graph: React.FC = () => {
   const graphDiv = useRef<HTMLDivElement>(null);
-  const [plotData, setPlotData] = useState([]);
-  const [actionsLayout, setActionLayout] = useState([]);
+  const [plotData, setPlotData] = useState<Partial<ScatterData>[]>([]);
+  const [actionsLayout, setActionLayout] = useState<Partial<Layout>>([] as Partial<Layout>);
+  const [selectedActions, setSelectedActions] = useState<string[]>([]);
+
+  const toggleVisibility = () => {
+    setSelectedActions((prevFilter) => (prevFilter.includes('Pulse Check') ? ['Order EKG'] : ['Pulse Check']));
+  };
 
   useEffect(() => {
     const url = "/api/actions/plotly/1Ae95pvrZsV32qBCSnCw_xkOKEw9BrI17";
@@ -16,30 +20,34 @@ const Graph: React.FC = () => {
       try {
         const response = await fetch(url);
         const json = await response.json();
-        const {data, layout} = json;
+        const {data, layout, actionGroups} = json;
         setPlotData(data);
         setActionLayout(layout);
+        setSelectedActions(actionGroups);
       } catch (error) {
         console.log("error", error);
       }
     };
 
-    fetchData().catch(e=>{console.log("error",e);});
+    fetchData().catch(console.error);
   }, []);
 
   useEffect(() => {
-    if (graphDiv.current && plotData.length>1) {
-      Plotly.newPlot(graphDiv.current, plotData as Partial<ScatterData>[], actionsLayout as Partial<Layout>, config).catch(console.error);
+    if (graphDiv.current && plotData && plotData.length>0) {
+      filterActions(selectedActions, plotData, actionsLayout);
+      // Apply filtering without re-rendering the entire chart
+      Plotly.react(graphDiv.current, plotData, actionsLayout, config).catch(console.error);
     }
+  }, [selectedActions, plotData, actionsLayout]);
 
-    return () => {
-      if (graphDiv.current) {
-        Plotly.purge(graphDiv.current);
-      }
-    };
-  }, [plotData, actionsLayout]);
-
-  return <div ref={graphDiv} style={{ width: "100%", height: "100%"}} />;
+  return (
+      <>
+        <button onClick={toggleVisibility}>
+          Toggle Pulse Check Visibility
+        </button>
+        <div ref={graphDiv} style={{width: "100%", height: "100%"}}/>
+      </>
+  );
 };
 
 export default Graph;
---
+++ src/filter-actions.ts
@@ -0,0 +1,57 @@
+import {Layout, ScatterData} from "plotly.js-basic-dist";
+
+function hide(index: number, series: Partial<ScatterData>, actionsLayout: Partial<Layout>) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    series.hoverinfo[index] = 'text';
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    series.textfont.color[index] = 'rgba(0,0,0,1)';
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    series.marker.opacity[index] = 1;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    actionsLayout.images[index].opacity = 1;
+}
+
+function show(index: number, series: Partial<ScatterData>, actionsLayout: Partial<Layout>) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    series.hoverinfo[index] = 'none';
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    series.textfont.color[index] = 'rgba(0,0,0,0)';
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    series.marker.opacity[index] = 0;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    actionsLayout.images[index].opacity = 0;
+}
+
+export default function filterActions(selectedActions: string[], plotData: Partial<ScatterData>[], actionsLayout: Partial<Layout>): {plotData: Partial<ScatterData>[], actionsLayout: Partial<Layout>} {
+    plotData.forEach(series => {
+        if (series.customdata && series.customdata.length > 0) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            series.hoverinfo = [];
+            series.textfont = series.textfont || {};
+            series.textfont.color = series.textfont.color || [];
+            series.marker = series.marker || {};
+            series.marker.color = series.marker.color|| [];
+            series.marker.opacity = series.marker.opacity || [];
+
+            series.customdata.forEach((value, index) => {
+                const shouldModify = selectedActions.includes(value as string);
+
+                if (shouldModify) {
+                    hide(index, series, actionsLayout);
+                } else {
+                    show(index, series, actionsLayout);
+                }
+            });
+        }
+    });
+    return {plotData:{...plotData}, actionsLayout: {...actionsLayout}};
+}
